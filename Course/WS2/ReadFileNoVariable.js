bf22083be73ea069f42c9506dea10df9b63d3de4
Jerojal <jert.ojala@gmail.com>
2023-03-23 20:27:15 +0200

    fs

--- Course/WS2/ReadFileNoVariable.js
+++ Course/WS2/ReadFileNoVariable.js
@@ -3,24 +3,25 @@ var fs = require("fs");
 console.log("Program started");
 
 
-fs.readFile('example.txt', results);
 
 for (var i = 0; i < 15; i++){
     console.log("Node just keeps on going while the file is being read...");
 }
-
+//non-anonymous function
+fs.readFile('example.txt', results);
 function results(err, data){
     if (err) return console.error(err);
     console.log("Results of fileread:")
     console.log(data.toString());
 }
-
-fs.readFile('example2.txt', results);
-function results(err, data){
+//anonymous function
+fs.readFile('example2.txt', function (err, data){
     if (err) return console.error(err);
     console.log("Results of fileread:")
     console.log(data.toString());
-}
+});
+
+
 
 fs.writeFile('combiningfiles.js')
 console.log("\nProgram Ended");
