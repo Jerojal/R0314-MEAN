93aa742638a7705d988a0e4303c75dcb23bfa8dd
Jerojal <jert.ojala@gmail.com>
2023-03-28 15:28:25 +0300

    sd

---
+++ Course/Project1GuestBook/routes.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/guestbook', function (req, res) {
+    var data = require('./JSONdataset.json');
+
+    var results = '<table border="1">';
+
+    for (var i=0; i < data.length; i++){
+        results +=
+        '<tr>'+
+        '<td>'+data[i].username+'</td>'+
+        '<td>'+data[i].country+'</td>'+
+        '<td>'+data[i].message+'</td>'+
+        '<td>'+data[i].date+'</td>'+
+        '</tr>';
+    }
+    res.send(results);
+
+});
+
+
+app.get('/newmessage', function (req, res) {
+    res.sendFile(__dirname + '/newmessage.html');
+});
+
+app.post('/newmessage', function (req, res) {   
+var data =require("./JSONdataset.json");
+
+data.push({
+    "username": req.body.username,
+    "country": req.body.country,
+    "message": req.body.message,
+    "date": new Date()
+ });
+ var jsonStr = JSON.stringify(data);
+
+ fs.writeFile('JSONdataset.json', jsonStr,  (err) => {
+    if (err) throw err;
+    console.log('It\'s saved!');
+});
+res.send("Saved the data to a file. Browse to /guestbook to see the results.")
+});
+app.get('/ajaxmessage', function (req, res) {
+    res.sendFile(__dirname + '/newmessage.html');
+});
+
+app.post('/ajaxmessage', function (req, res) {   
+var data =require("./JSONdataset.json");
+
+data.push({
+    "username": req.body.username,
+    "country": req.body.country,
+    "message": req.body.message,
+    "date": new Date()
+ });
+ var jsonStr = JSON.stringify(data);
+
+ fs.writeFile('JSONdataset.json', jsonStr,  (err) => {
+    if (err) throw err;
+    console.log('It\'s saved!');
+});
+res.send("Saved the data to a file. Browse to /guestbook to see the results.")
+});
+
+app.get('*', function (req, res) {
+    res.send('404');
+});
+
+app.listen(8081, function () {  
+    console.log('Example app listening on port 8081!');
+});
+
